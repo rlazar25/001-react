8cae25941c5bebbd4d08932f5687e88bfbda84d0
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-26 18:26:36 +0200

    Created NavbarComponent

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavbarComponent from './components/NavbarComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <NavbarComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,15 @@
+import '../css/navbarComponent.css';
+function NavbarComponent(){
+    return(
+        <div className="navbar">
+            <h1 className="title">Buy me</h1>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About us</li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavbarComponent;
